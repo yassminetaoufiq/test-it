b53a9fe8ca1bc696067f1a7b55c8da93d45a248a
TQ_YASS <e75807u@etu.univ-lorraine.fr>
2026-02-16 11:08:07 +0100

    ajout des roles

--- src/main/java/com/example/testit/security/SecurityConfig.java
+++ src/main/java/com/example/testit/security/SecurityConfig.java
@@ -1,26 +1,29 @@
-package com.example.testit.security;
+package com.example.testit.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
         http
-            .authorizeHttpRequests(authz -> authz
-                .anyRequest().authenticated()
-            )
-            .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            )
-            .httpBasic();
-
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authz -> {
+                authz
+                    .requestMatchers(HttpMethod.POST, "/**").hasAuthority("ADMIN")
+                    .requestMatchers(HttpMethod.GET, "/**").hasAnyAuthority("USER", "MANAGER")
+                    .anyRequest().authenticated();
+            })
+            .httpBasic(Customizer.withDefaults())
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        
         return http.build();
     }
 }
